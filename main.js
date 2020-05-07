bad6ec04cd9191b0e3b9f34524d4b3b798a9d1d7
Nicole Mirea <nicole@nimirea.com>
2020-05-07 10:43:35 -0500

    Ask for microphone permissions when needed
    - Don't ask on consent form page
    - Also adds an "end" to the experimen
    
    
    Former-commit-id: 97c9dd6ef69d739acba27bef7d0356c34e7542fc

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
+// config variables
+
 var app = new Vue({
   el: '#app',
   data: {
     stream: {},
     snd: {},
     recorder: {},
-    isPlaying: false,
+    isStarted: false,
     stimList: [{'twister': ''}],
-    currentStim: 0
+    currentStim: 0,
+    isi: 1000, // interstimulus interval, in ms
+    consentGiven: false,
+    rejected: false,
+    trialEnded: false,
+    expOver: false,
+    taskOrder: ['DDK', 'TT'],
+    currentTask: 0
   },
   created: function(){
     // get stimuli from CSV file
@@ -15,15 +24,42 @@ var app = new Vue({
       .then(data => {this.stimList = Papa.parse(data, {
         'header': true
       }).data});
-
-    // hook up microphone
-  	navigator.mediaDevices.getUserMedia({audio:true, video:false})
-    	.then((stream) => {
-    		this.stream = stream;
-    	})
   },
   methods : {
-  	playSound: function(){
+    // function that starts the experiment
+    startExp: function(){
+      // get microphone permissions before anything else
+      navigator.mediaDevices.getUserMedia({audio:true, video:false})
+      	.then((stream) => {
+      		this.stream = stream;
+
+          // update view to reflect that sound is playing
+      		this.isStarted = true;
+
+          // run first trial
+          this.runTrial();
+
+      	}).catch((err) => {
+          alert("In order to continue the experiment, you must allow this page to record audio using your microphone.")
+        })
+
+
+    },
+
+    // function that advances to next trial
+    nextTrial: function() {
+      // begin start of next trial (hide continue button)
+      this.trialEnded = false;
+
+      // advance stimulus
+      this.currentStim += 1;
+
+      // trial flow
+      this.runTrial();
+    },
+
+    // function that runs the trial
+  	runTrial: function(){
       // import metronome sound (4 slow + 12 fast beats) as an object
   		this.snd = new Pizzicato.Sound(
   			{
@@ -34,16 +70,36 @@ var app = new Vue({
   				}
   			},
 
-        // play metronome sound
-  			() => this.snd.play()
-  		);
+        // when sound is loaded...
+  			() => {
+          // set what to do after the sound ends
+          this.snd.on("end", () => setTimeout(() => {
+            this.recorder.stop();
+            // TODO upload recording here
+            // export to WAV file (locally; prompt for location)
+            // this.recorder.exportWAV((blob) => saveAs(blob, "recording.wav"));
 
-      // update view to reflect that sound is playing
-  		this.isPlaying = true;
+            // go to next item, if it exists
+            if (this.currentStim < this.stimList.length - 2) {
+              // show continue button
+              this.trialEnded = true;
+            } else {
+              this.endExp();
+            }
+          }, this.isi));
+
+          // actually play the sound, but after a 1s pause
+          setTimeout(() => {
+            this.snd.play();
+          }, this.isi);
+        }
+  		);
 
       // start recording
   		this.record();
   	},
+
+    // record the audio
   	record: function(){
   		var AudioContext = window.AudioContext || window.webkitAudioContext;
   		var audio_context = new AudioContext();
@@ -51,23 +107,15 @@ var app = new Vue({
   		this.recorder = new Recorder(input, {numChannels:1});
   		this.recorder.record();
   	},
-  	stop: function(){
 
-      // stop playing the sound
-	  	this.snd.stop();
+    // TODO play sample trial
+    sampleTrial: function() {
 
-      // stop the recorder
-  		this.recorder.stop();
+    },
 
-      // export to WAV file (locally; prompt for location)
-  		// this.recorder.exportWAV((blob) => saveAs(blob, "recording.wav"));
-
-      // update view as a result of this variable
-  		this.isPlaying = false;
-
-      // go to next twister
-      this.currentStim += 1;
-  	}
+    endExp: function() {
+      // update view
+      this.expOver = true;
+    }
   }
-
 })
